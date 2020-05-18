db94e6c31287f304be587df3c873fa2aeed450ee
MarioDorantes <62160690+MarioDorantes@users.noreply.github.com>
2020-05-17 21:35:13 -0500

    Actualizacion de la clase ProfessorValidations, package BussinessLogic

--- src/BusinessLogic/ProfessorValidations.java
+++ src/BusinessLogic/ProfessorValidations.java
@@ -1,20 +1,28 @@
+/*
+Intitution: Universidad Veracruzana 
+File creator: Mario Dorantes
+Class name: ProfessorValidations 
+Date of creation: April 22nd. 2020 
+*/
+
 package BusinessLogic;
 
-/**
- *
- * @author mario
- */
 public class ProfessorValidations extends PersonDataValidations {
     
     boolean validTurn; 
     boolean validQualification;
+    boolean validStaffNumber; 
     
     public boolean validateTurn (String turn) {
         return validTurn = turn.matches("^[M|m|V|v]{1}[a|t|u|i|n|o|e|s|p|r|t]*$"); 
     }
     
-    public boolean validateQualification (String qualification){
+    public boolean validateQualification (String qualification) {
         return validQualification = qualification.matches("^[0-9]{1,2}[.]{1}[0-9]{1,2}$"); 
+    }
+    
+    public boolean validateStaffNumber (String staffNumber) {
+        return validStaffNumber = staffNumber.matches("^[0-9]{5}$"); 
     }
     
 }
