6a8efc6f46a2d4a3aa26c54a3a22d4c930a937eb
gabrielflores <gabofl14@gmail.com>
2020-07-02 23:34:57 -0500

    Corrección de conflictos al realizar push/pull

--- src/Domain/MonthlyReport.java
+++ src/Domain/MonthlyReport.java
@@ -6,20 +6,13 @@ Date of creation: May 7th. 2020
 */
 package Domain;
 
-import java.io.File;
-import java.sql.Date;
 
 public class MonthlyReport {
     private int id_monthly;
-    private int monthNumber;
-    private int score;
-    private String observations;
-    private java.sql.Date dueDate;
-    private byte[] paperFile;
-    private String address;
+    private int id_practicing;
 
     public MonthlyReport(){
-        dueDate = new Date(System.currentTimeMillis());
+        
     }
 
     public int getId_monthly() {
@@ -30,57 +23,13 @@ public class MonthlyReport {
         this.id_monthly = id_monthly;
     }
 
-    public int getMonthNumber() {
-        return monthNumber;
+    public int getId_practicing() {
+        return id_practicing;
     }
 
-    public void setMonthNumber(int monthNumber) {
-        this.monthNumber = monthNumber;
+    public void setId_practicing(int id_practicing) {
+        this.id_practicing = id_practicing;
     }
 
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public String getObservations() {
-        return observations;
-    }
-
-    public void setObservations(String observations) {
-        this.observations = observations;
-    }
-
-    public Date getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public byte[] getPaperFile() {
-        return paperFile;
-    }
-
-    public void setPaperFile(byte[] paperFile) {
-        this.paperFile = paperFile;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void toBinary () {
-        File file = new File(address);
-        paperFile = new byte[(int) file.length()];
-        this.setPaperFile(paperFile);
-    }
+    
 }
